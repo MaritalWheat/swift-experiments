5bc7b814e4760baedc55a41cdf5d09a08f251e87
Emanuel Rosu <rosu.emanuel@gmail.com>
2015-07-26 18:20:39 -0700

    initial commit 'Stormy' lesson

---
+++ Stormy/Stormy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by Emanuel Rosu on 7/26/15.
+//  Copyright (c) 2015 Emanuel Rosu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
